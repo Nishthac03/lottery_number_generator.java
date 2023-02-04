4b103aa0c58fff45400200e83531cbf4d4251189
Nishtha Chopra <117111893+Nishthac03@users.noreply.github.com>
2023-02-04 22:42:01 +0530

    Create lottery_number_genrator.java

---
+++ lottery_number_genrator.java
@@ -0,0 +1,6 @@
+/*
+pick six numbers between 1 and 49
+after drawing a number,check if it is repeated in drawn numbers
+if repeated, loop the draw until it is not repeated
+sort the numbers using arrays.sort method
+*/
